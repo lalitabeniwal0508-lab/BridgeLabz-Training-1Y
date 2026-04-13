5008c185673f8de512c6feca7864ea0abcc546d1
lalitabeniwal0508-lab <lalitabeniwal0508@gmail.com>
2026-04-13 15:13:43 +0530

    Added: workshop

---
+++ workshop/src/com/gla/workshop/ReplaceZero.java
@@ -0,0 +1,16 @@
+package com.gla.workshop;
+
+public class ReplaceZero {
+    public static void main(String[] args) {
+            int num = 102030;
+
+            String str = String.valueOf(num);
+            str = str.replace('0', '1');
+
+            int result = Integer.parseInt(str);
+
+            System.out.println("Original number: " + num);
+            System.out.println("After replacing 0s with 1s: " + result);
+
+    }
+}
